7de0d1f85d582e24292714ed475652b475a40886
Blue <turnbf2@gmail.com>
2022-08-02 21:00:42 +0900

    v1

--- WebStudy-main/script.js
+++ WebStudy-main/script.js
@@ -13,16 +13,17 @@ let sliderData = {
     backgroundColor: "black",
     imgLink: "https://devinn.dev/@resource/images/index/kivi-techcon.svg",
   },
-};
+}; // 슬라이드 변경할 값들을 미리 객체로 저장
 
-let h1txt = document.querySelector("#slider-h1");
-let ptxt = document.querySelector("#slider-p");
-let imglink = document.querySelector("#slider-img");
-let backgroundcolor = document.querySelector(".ct-sliderbox");
-
+let h1txt = document.querySelector("#slider-h1"); //슬라이더값중 일부
+let ptxt = document.querySelector("#slider-p"); //슬라이더값중 일부
+let imglink = document.querySelector("#slider-img"); //슬라이더값중 일부
+let backgroundcolor = document.querySelector(".ct-sliderbox"); //슬라이더값중 일부
 
 function leftClick() {
+  // 좌클릭하면 슬라이더 아이템들의 값들을 변환
   if (h1txt.innerHTML === sliderData.one.h1Text) {
+    //h1.txt값을 비교하여 현재 슬라이더의 h1.txt값이 같으면 다른값으로 변환 아니라면 반대로.
     h1txt.innerHTML = sliderData.two.h1Text;
     ptxt.innerHTML = sliderData.two.pText;
     imglink.src = sliderData.two.imgLink;
@@ -33,10 +34,10 @@ function leftClick() {
     imglink.src = sliderData.one.imgLink;
     backgroundcolor.style.backgroundColor = sliderData.one.backgroundColor;
   }
-
 }
 
 function rightClick() {
+  // 우클릭하면 슬라이더 아이템들의 값들을 변환
   if (h1txt.innerHTML === sliderData.one.h1Text) {
     h1txt.innerHTML = sliderData.two.h1Text;
     ptxt.innerHTML = sliderData.two.pText;
